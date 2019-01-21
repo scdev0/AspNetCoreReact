02f94ea4629a721594c246b3b9ee14b197330e4f
scdev <28826596+scdev0@users.noreply.github.com>
2019-01-21 18:52:30 -0500

    Initial project commit; add gitignore and README

---
+++ API-Auth0/Controllers/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace APIAuth0.Controllers
+{
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            var currentUser = HttpContext.User;
+            var resultBookList = new Book[] {
+                new Book { Author = "Ray Bradbury", Title = "Fahrenheit 451", AgeRestriction = false },
+                new Book { Author = "Gabriel García Márquez", Title = "One Hundred years of Solitude", AgeRestriction = false },
+                new Book { Author = "George Orwell", Title = "1984", AgeRestriction = false },
+                new Book { Author = "Anais Nin", Title = "Delta of Venus", AgeRestriction = true }
+            };
+
+            return resultBookList;
+        }
+
+        public class Book
+        {
+            public string Author { get; set; }
+            public string Title { get; set; }
+            public bool AgeRestriction { get; set; }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# AspNetCoreReact
+
+This projects follows https://auth0.com/blog/developing-web-apps-with-asp-dot-net-core-2-dot-0-and-react-part-1/ for learning API-first Development and using .NET Core with React.
