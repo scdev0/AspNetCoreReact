286477e7386762091a3e9095f61d6f611eb8a664
scdev0 <28826596+scdev0@users.noreply.github.com>
2019-01-22 14:09:50 -0500

    Integrate auth0 and router to react app (#5)
    * Integrate auth0 to react app
    
    * Add router and fix login loop

--- react-auth0/src/App.js
+++ react-auth0/src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AuthService from './AuthService';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.authService = new AuthService();
+	}
+
+	renderHome() {
+		let resultComponent = <Home auth={this.authService} />;
+
+		if (!this.authService.isAuthenticated()) {
+			this.authService.login();
+			resultComponent = (
+				<div>
+					<p>Redirecting to the authentication service...</p>
+				</div>
+			);
+		}
+
+		return resultComponent;
+	}
+
+	startSession(history) {
+		this.authService.handleAuthentication(history);
+		return (
+			<div>
+				<p>Starting session...</p>
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>My Bookstore</p>
+				</header>
+				<Switch>
+					<Route exact path="/" render={() => this.renderHome()} />
+					<Route
+						path="/startSession"
+						render={({ history }) => this.startSession(history)}
+					/>
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ react-auth0/src/Auth0Config.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+	domain: 'scdev0.auth0.com',
+	clientID: 'YBVwMhY9AWnMtL63OYL7xR1NAL1f0XJb',
+	redirectUri: 'http://localhost:3000/startSession',
+	audience: 'https://onlinebookstore.mycompany.com'
+};
